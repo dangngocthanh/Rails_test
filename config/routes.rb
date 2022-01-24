08939479a0812fdd8ea5aa5fddc0ff14b51f8ba7
dang ngoc thanh <helomilo123@gmail.com>
2022-01-24 16:57:10 +0700

    Improve the README

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  # Allow connections to local server.
+  root 'application#hello'
+  config.hosts.clear
 end
