55eec269059adb4550248e2c2ff1137c5bce5d36
dang ngoc thanh <helomilo123@gmail.com>
2022-01-25 11:30:38 +0700

    Implement basic login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,8 +10,14 @@ class UsersController < ApplicationController
 
   def create
     p params['user']['password']
-    @user = User.new(email: params['user']['email'],name: params['user']['name'], password_digest: params['user']['password'])
+    cost = ActiveModel::SecurePassword.min_cost ?
+             BCrypt::Engine::MIN_COST :
+             BCrypt::Engine.cost
+    password = BCrypt::Password.create(params['user']['password'], cost: cost)
+
+    @user = User.new(email: params['user']['email'],name: params['user']['name'], password_digest: password)
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,12 @@ class User < ApplicationRecord
             uniqueness: true
   has_secure_password
   validates :password_digest, presence: true, length: { minimum: 6 }
+
+  def User.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ?
+             BCrypt::Engine::MIN_COST :
+             BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
+  end
+
 end
