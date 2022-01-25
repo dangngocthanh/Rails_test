63535207ad02d5181efa68ac4622c5d0cc2e3b0a
dang ngoc thanh <helomilo123@gmail.com>
2022-01-25 08:57:17 +0700

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,29 @@
 class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
   def new
+    @user = User.new
+  end
+
+  def create
+    p params['user']['password']
+    @user = User.new(email: params['user']['email'],name: params['user']['name'], password_digest: params['user']['password'])
+    if @user.save
+      flash[:success] = "Welcome to the Sample App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+
   end
 end
